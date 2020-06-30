14019ecb1085c5ec6e914b4d63301d8369a1bb81
Marcelo Madruga <m.madruga@cinq.com.br>
2020-06-30 16:49:49 -0400

    Initial commit

---
+++ src/main/java/com/cicd/pipeline/multibranch/MultiBranchApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.pipeline.multibranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiBranchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/pipeline/multibranch/MultiBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.pipeline.multibranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
